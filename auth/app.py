aea413d77d581f3449d269c9ce2b9bea9da5ea94
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2023-07-07 14:53:37 -0300

    Refactoring: remove class controller

--- auth/app.py
+++ auth/app.py
@@ -5,5 +5,5 @@ from auth.interfaces.controllers.user_controller import auth_bp
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(auth_bp, url_prefix='/v1')
+    app.register_blueprint(auth_bp, url_prefix='/v1/auth')
     return app
--- auth/interfaces/controllers/user_controller.py
+++ auth/interfaces/controllers/user_controller.py
@@ -21,76 +21,81 @@ auth_bp = Blueprint('auth', __name__)
 
 dotenv.load_dotenv()
 
+user_service = UserService(UserRepositoryImpl())
 
-class UserController:
-    def __init__(self):
-        self.user_service = UserService(UserRepositoryImpl())
 
-    @auth_bp.route('/users', methods=['GET'])
-    def get_users(self):
-        return GetUsersResponse(users=self.user_service.get_users()).to_dict()
+@auth_bp.route('/users', methods=['GET'])
+def get_users():
+    return GetUsersResponse(users=user_service.get_users()).to_dict()
 
-    @auth_bp.route('/users', methods=['POST'])
-    def create_user(self):
-        data = CreateUserRequest(data=request.get_json())
-        hashed_password = generate_password_hash(data.password)
-        user = self.user_service.create_user(
-            data.username, hashed_password, data.email
-        )
-        return CreateUserResponse(
-            username=user.username, email=user.email
-        ).to_dict()
 
-    @auth_bp.route('/login', methods=['POST'])
-    def login(self):
-        data = LoginRequest(data=request.get_json())
+@auth_bp.route('/users', methods=['POST'])
+def create_user():
+    data = CreateUserRequest(data=request.get_json())
+    hashed_password = generate_password_hash(data.password)
+    user = user_service.create_user(
+        data.username, hashed_password, data.email
+    )
+    return CreateUserResponse(
+        username=user.username, email=user.email
+    ).to_dict()
 
-        if data.username.endswith('@gmail.com'):
-            user = self.user_service.get_user_by_email(data.username)
-            if user and check_password_hash(user.password, data.password):
-                token = self.generate_token(user.username)
-                return LoginResponse(token=token).to_dict()
-        else:
-            user = self.user_service.get_user_by_username(data.username)
-            if user and check_password_hash(user.password, data.password):
-                token = self.generate_token(user.username)
-                return LoginResponse(token=token).to_dict()
 
-        return {'error': 'Invalid username or password'}, 401
+@auth_bp.route('/login', methods=['POST'])
+def login():
+    data = LoginRequest(data=request.get_json())
 
-    @auth_bp.route('/login-with-gmail', methods=['POST'])
-    def login_with_gmail(self):
-        data = LoginWithGmailRequest(data=request.get_json())
-
-        try:
-            token_info = self.verify_google_token(data.id_token)
-            email = token_info.get('email')
-            username = email.split('@')[0]
-            user = self.user_service.get_user_by_email(email)
-
-            if not user:
-                password = generate_password_hash('gmailuser')
-                user = self.user_service.create_user(username, password, email)
-
-            token = self.generate_token(username)
+    if data.username.endswith('@gmail.com'):
+        user = user_service.get_user_by_email(data.username)
+        if user and check_password_hash(user.password, data.password):
+            token = generate_token(user.username)
+            return LoginResponse(token=token).to_dict()
+    else:
+        user = user_service.get_user_by_username(
+            data.username)
+        if user and check_password_hash(user.password, data.password):
+            token = generate_token(user.username)
             return LoginResponse(token=token).to_dict()
 
-        except ValueError:
-            return {'error': 'Invalid Google token'}, 401
+    return {'error': 'Invalid username or password'}, 401
 
-    def verify_google_token(self, id_token):
-        return id_token.verify_oauth2_token(
-            id_token,
-            google_requests.Request(),
-            dotenv.get_key('.env', 'GOOGLE_CLIENT_ID'),
-        )
 
-    def generate_token(self, username):
-        payload = {
-            'username': username,
-            'exp': datetime.utcnow() + timedelta(hours=1),
-        }
-        token = jwt.encode(
-            payload, dotenv.get_key('.env', 'SECRET_KEY'), algorithm='HS256'
-        )
-        return token.decode('utf-8')
+@auth_bp.route('/login-with-gmail', methods=['POST'])
+def login_with_gmail():
+    data = LoginWithGmailRequest(data=request.get_json())
+
+    try:
+        token_info = verify_google_token(data.id_token)
+        email = token_info.get('email')
+        username = email.split('@')[0]
+        user = user_service.get_user_by_email(email)
+
+        if not user:
+            password = generate_password_hash('gmailuser')
+            user = user_service.create_user(
+                username, password, email)
+
+        token = generate_token(username)
+        return LoginResponse(token=token).to_dict()
+
+    except ValueError:
+        return {'error': 'Invalid Google token'}, 401
+
+
+def verify_google_token(id_token):
+    return id_token.verify_oauth2_token(
+        id_token,
+        google_requests.Request(),
+        dotenv.get_key('.env', 'GOOGLE_CLIENT_ID'),
+    )
+
+
+def generate_token(username):
+    payload = {
+        'username': username,
+        'exp': datetime.utcnow() + timedelta(hours=1),
+    }
+    token = jwt.encode(
+        payload, dotenv.get_key('.env', 'SECRET_KEY'), algorithm='HS256'
+    )
+    return token.decode('utf-8')
