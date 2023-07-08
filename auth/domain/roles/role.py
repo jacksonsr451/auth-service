a6ac88a248c44ad1397b9009e37e82d32a712453
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2023-07-08 16:45:34 -0300

    feat: include validade in role

--- auth/domain/roles/role.py
+++ auth/domain/roles/role.py
@@ -1,3 +1,15 @@
+from auth.domain.roles.role_enum import RoleEnum
+
+
 class Role:
     def __init__(self, name):
-        self.name = name
+        self.name = self.__validate_role(name=name)
+
+    class Config:
+        validate_assignment = True
+
+    def __validate_role(self, name) -> str:
+        valid_names = [role.value for role in RoleEnum]
+        if name not in valid_names:
+            raise ValueError("Invalid role")
+        return name
---
+++ auth/domain/roles/role_enum.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class RoleEnum(Enum):
+    super_admin = "super_admin"
+    admin = "admin"
+    user = "user"
